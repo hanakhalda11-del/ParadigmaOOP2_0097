869558d7fdca356efdb3456eac188377bac6a25b
hanakhalda11-del <hanakhalda11@gmail.com>
2026-05-19 16:08:38 +0700

    progra fix

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -13,4 +13,18 @@ public:
         x = a;
         y = b;
     }
-    
+    //menampilkan nilai
+    void display()
+    {
+        cout << "x = " << x <<endl;
+        cout << "y = " << y <<endl;
+    }
+};
+int main()
+{
+    AbstraksiKlas ak;
+    ak.setXY("Yogyakarta", "Kampus");
+    ak.display();
+
+    return 0;
+}
