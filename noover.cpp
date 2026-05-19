3ae67866c874af243f091dc8a3c317c6439084c2
hanakhalda11-del <hanakhalda11@gmail.com>
2026-05-19 16:03:35 +0700

    output program

--- noover.cpp
+++ noover.cpp
@@ -9,18 +9,3 @@ public:
         cout << "halo, saya function dari base class";
     }
 };
-class derivedClass : public baseClass
-{
-public:
-    void perkenalan1()
-    {
-        cout << "halo, saya function dari derived Class";
-    }
-};
-int main()
-{
-    derivedClass a;
-    a.perkenalan1();
-
-    return 0;
-}
